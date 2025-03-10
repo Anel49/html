214c23893451f0b265f3bd7dde3bba1c3cbc5026
Anel49 <andels4949@students.ecpi.edu>
2025-03-10 11:08:20 -0400

    added shopping cart icon (plans to make it functional), adds items to localStorage and converts the strings to workable array with dictionary objects

--- frontend/product.js
+++ frontend/product.js
@@ -1,7 +1,8 @@
 
 // variables for cart totals
 let subtotal = 0, taxed = 0, total = 0;
-// array of dictionary objects with full size name, size letter, and price
+let shoppingCartItems = [];
+// array of dictionary objects with full size names, size letters, and prices
 let sizePrices = [
     {
         size: "Extra Small",
@@ -40,22 +41,52 @@ let sizePrices = [
         availability: "In Stock"
     }
 ]
-
 addSelectOptions();
 
 function preventPageReload(e){
     e.preventDefault();
 }
-document.getElementById("add-to-cart-btn").addEventListener(this, preventPageReload);
+document.getElementById("add-to-cart-btn").addEventListener(
+    this, preventPageReload);
+
+// grabs localStorage strings and converts them to dict objects for 
+// easier operations
+function updateCartArr(){
+    shoppingCartItems = [];
+    for (let i = 0; i < localStorage.length; i++){
+    
+        let workingDict = {};
+        let workingStr = "";
+        let str = localStorage.getItem("cartItem" + i);
+    
+        workingStr = str.split(",");
+        workingStr[2] = Number(workingStr[2]);
+    
+        workingDict['size'] = workingStr[0];
+        workingDict['letter'] = workingStr[1];
+        workingDict['price'] = workingStr[2];
+    
+        shoppingCartItems.push(workingDict);
+
+        subtotal += shoppingCartItems[i]['price'];
+    }
+    console.log(shoppingCartItems);
+}
 
 // grabs the selected size and finds its price
-function sizeSelection(size){
+function sizeSelection(selectedSize){
     let price = 0;
     let productSize = "";
     for (let i = 0; i < sizePrices.length; i++){
-        if (sizePrices[i]['letter'] == size){
+        if (sizePrices[i]['letter'] == selectedSize){
             price = sizePrices[i]['price'];
             productSize = sizePrices[i]['letter'];
+
+            let storageStr = [sizePrices[i]['size'], sizePrices[i]['letter'], 
+            sizePrices[i]['price']].join(",");
+
+            localStorage.setItem("cartItem" + localStorage.length, storageStr);
+            updateCartArr();
         }
     }
     addItemToCart(price, productSize);
@@ -68,9 +99,8 @@ function addItemToCart(price, productSize){
     alert(productSize + " Shirt Added (Costs $" + price + ") - Total $" + taxed.toFixed(2));
 }
 
-// creates the size dropdown menu based on sizePrices dictionary
+// creates the table and dropdown menu based from dictionary
 function addSelectOptions(){
-
     // sizes and availability table
     const sizes = document.getElementById("sizes-table");
     sizes.innerHTML = 
@@ -89,7 +119,6 @@ function addSelectOptions(){
             <tr>
             `
     }
-
     // select box
     const selects = document.getElementById("selects");
     selects.innerHTML = "";
